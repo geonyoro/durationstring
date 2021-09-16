e082c1bcc27fd59c3af3c859a2125b0ac2acd77f
George Nyoro <geonyoro@gmail.com>
2021-09-16 07:21:42 +0300

    Added Setup for Duration

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='durationstring',
+    version='0.0.1',    
+    description='A example Python package',
+    url='https://github.com/geonyoro/durationstring',
+    author='George Nyoro',
+    author_email='geonyoro@gmail.com',
+    license='Apache',
+    packages=['durationstring'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License',  
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft'
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
